5b85609216f2e357395144e9ec0b28672f1ffddf
guohuadeng <guohuadeng@hotmail.com>
2015-03-26 21:47:49 +0800

    接口文档v1.2，先用临时接口

---
+++ magento-server-kikuu/getdeallist.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Example of retrieving the products list using Admin account via Magento REST API. OAuth authorization is used
+ * Preconditions:
+ * 1. Install php oauth extension
+ * 2. If you were authorized as a Customer before this step, clear browser cookies for 'yourhost'
+ * 3. Create at least one product in Magento
+ * 4. Configure resource permissions for Admin REST user for retrieving all product data for Admin
+ * 5. Create a Consumer
+ */
+// $callbackUrl is a path to your file with OAuth authentication example for the Admin user
+$callbackUrl = "http://skymazon.sunpop.cn/getdeallist.php";
+$temporaryCredentialsRequestUrl = "http://clean.sunpop.cn/oauth/initiate?oauth_callback=" . urlencode($callbackUrl);
+$adminAuthorizationUrl = 'http://clean.sunpop.cn/admin/oauth_authorize';
+$accessTokenRequestUrl = 'http://clean.sunpop.cn/oauth/token';
+$apiUrl = 'http://clean.sunpop.cn/api/rest';
+$consumerKey = 'a9e7c35ab6f8d778cf57d67ca2362011';
+$consumerSecret = 'cb0f1f0680867b2a4bd69065ce2989e7';
+
+session_start();
+if (!isset($_GET['oauth_token']) && isset($_SESSION['state']) && $_SESSION['state'] == 1) {
+    $_SESSION['state'] = 0;
+}
+try {
+    $authType = ($_SESSION['state'] == 2) ? OAUTH_AUTH_TYPE_AUTHORIZATION : OAUTH_AUTH_TYPE_URI;
+    $oauthClient = new OAuth($consumerKey, $consumerSecret, OAUTH_SIG_METHOD_HMACSHA1, $authType);
+    $oauthClient->enableDebug();
+
+    if (!isset($_GET['oauth_token']) && !$_SESSION['state']) {
+        $requestToken = $oauthClient->getRequestToken($temporaryCredentialsRequestUrl);
+        $_SESSION['secret'] = $requestToken['oauth_token_secret'];
+        $_SESSION['state'] = 1;
+        header('Location: ' . $adminAuthorizationUrl . '?oauth_token=' . $requestToken['oauth_token']);
+        exit;
+    } else if ($_SESSION['state'] == 1) {
+        $oauthClient->setToken($_GET['oauth_token'], $_SESSION['secret']);
+        $accessToken = $oauthClient->getAccessToken($accessTokenRequestUrl);
+        $_SESSION['state'] = 2;
+        $_SESSION['token'] = $accessToken['oauth_token'];
+        $_SESSION['secret'] = $accessToken['oauth_token_secret'];
+        header('Location: ' . $callbackUrl);
+        exit;
+    } else {
+        $oauthClient->setToken($_SESSION['token'], $_SESSION['secret']);
+
+        $resourceUrl = "$apiUrl/products?page=1&limit=5&filter[1][attribute]=special_price&filter[1][gt]=0";
+        $oauthClient->fetch($resourceUrl, array(), 'GET', array('Content-Type' => 'application/json'));
+        $productsList = json_decode($oauthClient->getLastResponse());
+        print_r($productsList);
+    }
+} catch (OAuthException $e) {
+    print_r($e->getMessage());
+    echo "<br/>";
+    print_r($e->lastResponse);
+}
