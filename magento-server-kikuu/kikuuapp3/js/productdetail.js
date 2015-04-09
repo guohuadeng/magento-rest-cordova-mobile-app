acc902c4288f6ef9fb37ed4f66a3ca12646c1d21
guohuadeng <guohuadeng@hotmail.com>
2015-04-10 05:24:28 +0800

    productdetail css update
    eg:
    
    http://skymazon.sunpop.cn/kikuuapp3/productDetail.html?entity_id=431&title=Top

--- magento-server-kikuu/kikuuapp3/js/productdetail.js
+++ magento-server-kikuu/kikuuapp3/js/productdetail.js
@@ -17,57 +17,51 @@ function requestUrl(paras)
 	
 var entity_id = requestUrl('entity_id') ;
 var title = requestUrl('title') ;
-var swiper = new Swiper('.swiper-container', {
-	pagination: '.product-swiper-pagination',
-    slidesPerView: 1,
-	centeredSlides: true,
-	parallax: true,
-	paginationClickable: true
+var swiper = new Swiper('.product-swiper-container', {
+	pagination: '.swiper-pagination',
+	paginationClickable: true,
 });
 //初始化页面中产品信息
 $(document).ready(function(){
 	var $detailTpl = $('#detail-template');	
 	var $detailImgTpl = $('#detailImg-template');	
 	$('.title').text(title);
-		
-	//--------	
-	$('#btn1').click(function(){
-		//产品图片列表
-		$.ajax ({
-				type : 'get',
-				url: baseUrl + '/api/rest/products/' + entity_id + '/images',
-				dataType: 'json', // 注意：JSONP <-- P (lowercase)
-				success: function(imglist) {
-					$('.swiper-wrapper').html(Handlebars.compile($detailImgTpl.html())({
-						imglist: imglist
-					}));
-                if (callback) {
-                    callback();
-                }
-            },
-				error: function (jqXHR) {
-					alert('Please check the network!');
+
+	//产品图片列表
+	$.ajax ({
+			type : 'get',
+			url: baseUrl + '/api/rest/products/' + entity_id + '/images',
+			dataType: 'json', // 注意：JSONP <-- P (lowercase)
+			success: function(imglist) {
+				$.each(imglist, function (i, item) {
+					swiper.appendSlide(sprintf(
+						'<div class="swiper-slide"><img src="%s"></div>',
+						item.url));
+					});
+			if (callback) {
+				callback();
+			}
+		},
+			error: function (jqXHR) {
+				alert('Please check the network!');
+			}			
+		});	
+//--------	
+	//产品详情
+	$.ajax ({
+			type : 'get',
+			url: baseUrl+'/api/rest/products/' + entity_id + '/',
+			dataType: 'json', // 注意：JSONP <-- P (lowercase)
+			success:function(product){	
+				$('.title').text(product.name);
+				//将json对象用刚刚注册的Handlebars模版封装，得到最终的html，插入到基础productInfo中。
+				$('#productInfo').html(Handlebars.compile($detailTpl.html())({
+					product: product
+				}));
+				},
+			error: function (jqXHR) {
+				alert('Please check the network!');
 				}			
 			});
-		})
-	$('#btn2').click(function(){
-		//产品详情
-		$.ajax ({
-				type : 'get',
-				url: baseUrl+'/api/rest/products/' + entity_id + '/',
-				dataType: 'json', // 注意：JSONP <-- P (lowercase)
-				success:function(product){	
-					$('.title').text(product.name);
-					//将json对象用刚刚注册的Handlebars模版封装，得到最终的html，插入到基础productInfo中。
-					$('#productInfo').html(Handlebars.compile($detailTpl.html())({
-						product: product
-					}));
-					},
-				error: function (jqXHR) {
-					alert('Please check the network!');
-					}			
-				});
-		//end 详情
-	//----	
-	});
+	//end 详情
 });
