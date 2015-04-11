a015d54614938e380afac29a9d74bf9a6bb25d32
guohuadeng <guohuadeng@hotmail.com>
2015-04-12 07:37:45 +0800

    add fastclick,productdetail, change ajax count
    使用fastclick体验好，但为开发方便暂时注释。
    产品详情页更新
    ajax取产品时固定数量

--- magento-server-kikuu/kikuuapp3/js/productdetail.js
+++ magento-server-kikuu/kikuuapp3/js/productdetail.js
@@ -1,31 +1,14 @@
-// 处理页面url传递的参数
-function requestUrl(paras)
-    { 
-        var url = location.href; 
-        var paraString = url.substring(url.indexOf('?')+1,url.length).split('&'); 
-        var paraObj = {} 
-        for (i=0; j=paraString[i]; i++){ 
-        paraObj[j.substring(0,j.indexOf('=')).toLowerCase()] = j.substring(j.indexOf('=')+1,j.length); 
-        } 
-        var returnValue = paraObj[paras.toLowerCase()]; 
-        if(typeof(returnValue)=='undefined'){ 
-        return ''; 
-        }else{ 
-        return returnValue; 
-        } 
-    }
 	
-var entity_id = requestUrl('entity_id') ;
-var title = requestUrl('title') ;
-var swiper = new Swiper('.product-swiper-container', {
-	pagination: '.swiper-pagination',
-	paginationClickable: true,
-});
-//初始化页面中产品信息
-$(document).ready(function(){
-	var $detailTpl = $('#detail-template');	
-	var $detailImgTpl = $('#detailImg-template');	
-	$('.title').text(title);
+//初始化页面中产品
+function showProductPage() {
+	var entity_id = requestUrl('entity_id') ;
+	var title = requestUrl('title') ;
+	var $productDetailTpl = $('#productDetail-template');	
+	var productSwiper = new Swiper('.product-swiper-container', {
+		pagination: '.product-swiper-container .swiper-pagination',
+		paginationClickable: true,
+		loop: true
+	});
 
 	//产品图片列表
 	$.ajax ({
@@ -33,29 +16,33 @@ $(document).ready(function(){
 			url: baseUrl + '/api/rest/products/' + entity_id + '/images',
 			dataType: 'json', // 注意：JSONP <-- P (lowercase)
 			success: function(imglist) {
-				$.each(imglist, function (i, item) {
-					swiper.appendSlide(sprintf(
+				$.each(imglist, function (i, item) {					
+		<!-- 详情 rest 图片生成 -->
+					productSwiper.appendSlide(sprintf(
 						'<div class="swiper-slide"><img src="%s"></div>',
 						item.url));
 					});
-			if (callback) {
-				callback();
-			}
+					productSwiper.slideTo(1);
+                    $('img.lazy').lazyload({
+                        effect: 'fadeIn',
+                        container: ('.swiper-slide'),
+                        placeholder: 'images/loading.gif'
+                    });
 		},
 			error: function (jqXHR) {
 				alert('Please check the network!');
 			}			
 		});	
-//--------	
+//--------	*/
 	//产品详情
 	$.ajax ({
 			type : 'get',
 			url: baseUrl+'/api/rest/products/' + entity_id + '/',
 			dataType: 'json', // 注意：JSONP <-- P (lowercase)
 			success:function(product){	
-				$('.title').text(product.name);
+				//$('.title').text(product.name);
 				//将json对象用刚刚注册的Handlebars模版封装，得到最终的html，插入到基础productInfo中。
-				$('#productInfo').html(Handlebars.compile($detailTpl.html())({
+				$('#productInfo').html(Handlebars.compile($productDetailTpl.html())({
 					product: product
 				}));
 				},
@@ -64,4 +51,4 @@ $(document).ready(function(){
 				}			
 			});
 	//end 详情
-});
+}
