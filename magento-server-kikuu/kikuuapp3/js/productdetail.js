ce002fed562fb6d0e5ac154e89d18b81988549d8
guohuadeng <guohuadeng@hotmail.com>
2015-04-12 11:42:24 +0800

    product update

--- magento-server-kikuu/kikuuapp3/js/productdetail.js
+++ magento-server-kikuu/kikuuapp3/js/productdetail.js
@@ -1,16 +1,21 @@
-	
+var productSwiper,		//产品详情页中的图片
+	entity_id;			//当前查看产品
+$productDetailTpl = $('#productDetail-template');	
 //初始化页面中产品
-function showProductPage() {
-	var entity_id = requestUrl('entity_id') ;
-	var title = requestUrl('title') ;
-	var $productDetailTpl = $('#productDetail-template');	
-	var productSwiper = new Swiper('.product-swiper-container', {
-		pagination: '.product-swiper-container .swiper-pagination',
-		paginationClickable: true,
-		loop: true
-	});
 
-	//产品图片列表
+function setProductId (id)	{ entity_id = id; };
+
+function showProductPage() {
+	//var entity_id = requestUrl('entity_id') ;
+	//产品图片列表，这个必须在上面的产品详情已经构建完成后才执行
+	if (productSwiper) {
+		}
+	else {
+		productSwiper = new Swiper('.product-swiper-container', {
+			pagination: '.product-swiper-container .swiper-pagination',
+			paginationClickable: true
+			});	
+		}
 	$.ajax ({
 			type : 'get',
 			url: baseUrl + '/api/rest/products/' + entity_id + '/images',
@@ -18,6 +23,7 @@ function showProductPage() {
 			success: function(imglist) {
 				$.each(imglist, function (i, item) {					
 		<!-- 详情 rest 图片生成 -->
+					productSwiper.removeAllSlides();
 					productSwiper.appendSlide(sprintf(
 						'<div class="swiper-slide"><img src="%s"></div>',
 						item.url));
@@ -40,7 +46,6 @@ function showProductPage() {
 			url: baseUrl+'/api/rest/products/' + entity_id + '/',
 			dataType: 'json', // 注意：JSONP <-- P (lowercase)
 			success:function(product){	
-				//$('.title').text(product.name);
 				//将json对象用刚刚注册的Handlebars模版封装，得到最终的html，插入到基础productInfo中。
 				$('#productInfo').html(Handlebars.compile($productDetailTpl.html())({
 					product: product
@@ -51,4 +56,5 @@ function showProductPage() {
 				}			
 			});
 	//end 详情
+	//productSwiper = null;
 }
