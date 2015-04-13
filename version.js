78a2e7a72e40aaf5656cf0609c5ef1acba93be60
zhixin <wenzhixin2010@gmail.com>
2015-04-14 00:23:28 +0800

    Add version build

---
+++ version.js
@@ -0,0 +1,8 @@
+var fs = require('fs'),
+    version = require('./version.json'),
+    manifestFile = './android/Kikuu/AndroidManifest.xml',
+    manifest = fs.readFileSync(manifestFile).toString();
+
+manifest = manifest.replace(/android:versionCode="(\d*)"/, 'android:versionCode="' + version.code + '"');
+manifest = manifest.replace(/android:versionName="([\d.]*)"/, 'android:versionName="' + version.name + '"');
+fs.writeFileSync(manifestFile, manifest);
