693cdd13a785762be0597ce431055f5746e19413
guohuadeng <guohuadeng@hotmail.com>
2015-05-08 19:01:25 +0800

    working on product detail

--- magento-server-kikuu/app/code/local/SkyMazon/RestConnect/controllers/ProductsController.php
+++ magento-server-kikuu/app/code/local/SkyMazon/RestConnect/controllers/ProductsController.php
@@ -8,16 +8,16 @@ class SkyMazon_RestConnect_ProductsController extends Mage_Core_Controller_Front
 		$selectid = 1;
 		$select = array ();
 		foreach ( $product->getOptions () as $o ) {
-			if (($o->getType () == "field") || ($o->getType () =="file")) {
+			if (($o->getType () == "field") || ($o->getType () == "file")) {
 				$select [$selectid] = array (
 						'option_id' => $o->getId (),
 						'custom_option_type' => $o->getType (),
 						'custom_option_title' => $o->getTitle (),
 						'is_require' => $o->getIsRequire (),
 						'price' => number_format ( Mage::helper ( 'directory' )->currencyConvert ( $o->getPrice (), $baseCurrency, $currentCurrency ), 2, '.', '' ),
-						'price_type'=>$o->getPriceType(),
-						'sku'=>$o->getSku(),
-						'max_characters' => $o->getMaxCharacters (),
+						'price_type' => $o->getPriceType (),
+						'sku' => $o->getSku (),
+						'max_characters' => $o->getMaxCharacters () 
 				);
 			} else {
 				$max_characters = $o->getMaxCharacters ();
@@ -26,6 +26,12 @@ class SkyMazon_RestConnect_ProductsController extends Mage_Core_Controller_Front
 				$values = $o->getValues ();
 				foreach ( $values as $v ) {
 					$options [$optionid] = $v->getData ();
+					if(null!==$v->getData('price') && null!==$v->getData('default_price')){
+						$options [$optionid]['price']=number_format ( Mage::helper ( 'directory' )->currencyConvert ( $v->getPrice (), $baseCurrency, $currentCurrency ), 2, '.', '' );
+						$options [$optionid]['default_price']=number_format ( Mage::helper ( 'directory' )->currencyConvert ( $v->getDefaultPrice (), $baseCurrency, $currentCurrency ), 2, '.', '' );
+					}
+						
+					
 					$optionid ++;
 				}
 				$select [$selectid] = array (
@@ -50,10 +56,10 @@ class SkyMazon_RestConnect_ProductsController extends Mage_Core_Controller_Front
 		$currentCurrency = Mage::app ()->getStore ()->getCurrentCurrencyCode ();
 		$productid = $this->getRequest ()->getParam ( 'productid' );
 		$product = Mage::getModel ( "catalog/product" )->load ( $productid );
-		if ($product->getOptions())
-				$has_custom_options = true;
+		if ($product->getOptions ())
+			$has_custom_options = true;
 		else
-				$has_custom_options = false;
+			$has_custom_options = false;
 		
 		$productdetail = array (
 				'entity_id' => $product->getId (),
@@ -74,18 +80,45 @@ class SkyMazon_RestConnect_ProductsController extends Mage_Core_Controller_Front
 		);
 		echo json_encode ( $productdetail );
 	}
-
-	public function getPicListsAction(){
+	public function getPicListsAction() {
 		$productId = ( int ) $this->getRequest ()->getParam ( 'product' );
-		$_product=Mage::getModel ( "catalog/product" )->load ( $productid );
-		$_images = Mage::getModel('catalog/product')->load($productId)->getMediaGalleryImages();
-		$images=array();
-		foreach ($_images as $_image){
-			$images[]=array('url'=>$_image->getUrl(),
-					'position'=>$_image->getPosition()
+		$_product = Mage::getModel ( "catalog/product" )->load ( $productid );
+		$_images = Mage::getModel ( 'catalog/product' )->load ( $productId )->getMediaGalleryImages ();
+		$images = array ();
+		foreach ( $_images as $_image ) {
+			$images [] = array (
+					'url' => $_image->getUrl (),
+					'position' => $_image->getPosition () 
 			);
 		}
-		//var_dump($images);
-		echo json_encode($images);
+		echo json_encode ( $images );
+	}
+	public function getAditionalAction(array $excludeAttr = array()) {
+		$data = array ();
+		$productId = ( int ) $this->getRequest ()->getParam ( 'product' );
+		$product = Mage::getModel ( "catalog/product" )->load ( $productid );
+		$attributes = $product->getAttributes ();
+		foreach ( $attributes as $attribute ) {
+			if ($attribute->getIsVisibleOnFront () && ! in_array ( $attribute->getAttributeCode (), $excludeAttr )) {
+				$value = $attribute->getFrontend ()->getValue ( $product );
+				
+				if (! $product->hasData ( $attribute->getAttributeCode () )) {
+					$value = Mage::helper ( 'catalog' )->__ ( 'N/A' );
+				} elseif (( string ) $value == '') {
+					$value = Mage::helper ( 'catalog' )->__ ( 'No' );
+				} elseif ($attribute->getFrontendInput () == 'price' && is_string ( $value )) {
+					$value = Mage::app ()->getStore ()->convertPrice ( $value, true );
+				}
+				
+				if (is_string ( $value ) && strlen ( $value )) {
+					$data [$attribute->getAttributeCode ()] = array (
+							'label' => $attribute->getStoreLabel (),
+							'value' => $value,
+							'code' => $attribute->getAttributeCode () 
+					);
+				}
+			}
+		}
+		echo var_dump($data);
 	}
 } 
