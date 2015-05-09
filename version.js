ecdd5e135f79ce1374afbc59ea2514d091c9f04d
zhixin <wenzhixin2010@gmail.com>
2015-05-09 13:08:12 +0800

    Update to release build.

--- version.js
+++ version.js
@@ -1,6 +1,7 @@
 var fs = require('fs'),
     version = require('./version.json'),
-    manifestFile = './android/Kikuu/AndroidManifest.xml',
+    //manifestFile = './android/Kikuu/AndroidManifest.xml',
+    manifestFile = './magento-server-kikuu/kikuu/platforms/android/AndroidManifest.xml';
     manifest = fs.readFileSync(manifestFile).toString();
 
 manifest = manifest.replace(/android:versionCode="(\d*)"/, 'android:versionCode="' + version.code + '"');
