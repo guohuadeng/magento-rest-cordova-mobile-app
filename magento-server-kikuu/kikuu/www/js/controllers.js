147b8fd74e6dcad60bb4017f53a30b3c6d2f2d8a
guohuadeng <guohuadeng@hotmail.com>
2015-05-20 04:21:29 +0800

    build

--- magento-server-kikuu/kikuu/www/js/controllers.js
+++ magento-server-kikuu/kikuu/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('app.controllers', [])
 
-    .controller('AppCtrl', function ($scope, $rootScope, $ionicModal, $ionicTabsDelegate, $ionicLoading, $timeout) {
+    .controller('AppCtrl', function ($scope, $rootScope, $ionicModal, $ionicTabsDelegate, $ionicLoading, $ionicPopup, $timeout) {
         //取数据时的loading mask
         $scope.showLoading = function () {
             $ionicLoading.show({
@@ -10,6 +10,68 @@ angular.module('app.controllers', [])
         $scope.hideLoading = function () {
             $ionicLoading.hide();
         };
+				
+				//各种弹出信息				
+				// Triggered on a button click, or some other target
+				$scope.showPopup = function() {
+					$scope.data = {}
+				
+					// An elaborate, custom popup
+					var myPopup = $ionicPopup.show({
+						template: '<input type="password" ng-model="data.wifi">',
+						title: 'Enter Wi-Fi Password',
+						subTitle: 'Please use normal things',
+						scope: $scope,
+						buttons: [
+							{ text: 'Cancel' },
+							{
+								text: '<b>Save</b>',
+								type: 'button-positive',
+								onTap: function(e) {
+									if (!$scope.data.wifi) {
+										//don't allow the user to close unless he enters wifi password
+										e.preventDefault();
+									} else {
+										return $scope.data.wifi;
+									}
+								}
+							}
+						]
+					});
+					myPopup.then(function(res) {
+						console.log('Tapped!', res);
+					});
+					$timeout(function() {
+						 myPopup.close(); //close the popup after 3 seconds for some reason
+					}, 3000);
+				 };
+				 // A confirm dialog
+				 $scope.showConfirm = function() {
+					 var confirmPopup = $ionicPopup.confirm({
+						 title: 'Consume Ice Cream',
+						 template: 'Are you sure you want to eat this ice cream?'
+					 });
+					 confirmPopup.then(function(res) {
+						 if(res) {
+							 console.log('You are sure');
+						 } else {
+							 console.log('You are not sure');
+						 }
+					 });
+				 };
+				
+				 // An alert dialog
+				 $scope.showAlert = function() {
+					 var alertPopup = $ionicPopup.alert({
+						 title: 'Don\'t eat that!',
+						 template: 'It might taste good'
+					 });
+					 alertPopup.then(function(res) {
+						 console.log('Thank you for not eating my delicious ice cream cone');
+					 });
+				 };
+
+			//end 各种弹出信息
         // 网站列表信息
         $scope.getWebsite = function () {
             $rootScope.service.get('website', function (website) {
@@ -139,7 +201,7 @@ angular.module('app.controllers', [])
             }
         };
     })
-
+		//列表
     .controller('ListsCtrl', function ($scope, $rootScope) {
         var getList = function (tab, type, callback) {
             if (type === 'load') {
